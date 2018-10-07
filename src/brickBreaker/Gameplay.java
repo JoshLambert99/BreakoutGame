e93ec04d3a90133807cfde6693fe052924fd17d1
Josh Lambert <joshl085@hotmail.com>
2018-10-07 12:35:57 +0100

    secondGo

--- src/brickBreaker/Gameplay.java
+++ src/brickBreaker/Gameplay.java
@@ -104,6 +104,18 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 			playerX-= 20; //move
 		
 	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 		
 	
 }
